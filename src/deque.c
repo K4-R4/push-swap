57dfd02ffe4cd4f42a4bfa5610bc5bfca6084741
K4R4 <mabdman512@gmail.com>
2023-07-09 23:08:23 +0900

    deque funcs fully implemented

--- src/deque.c
+++ src/deque.c
@@ -6,7 +6,7 @@
 /*   By: tkuramot <tkuramot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 21:36:51 by tkuramot          #+#    #+#             */
-/*   Updated: 2023/07/09 23:02:16 by tkuramot         ###   ########.fr       */
+/*   Updated: 2023/07/09 23:08:00 by tkuramot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,46 +30,6 @@ bool	deque_is_empty(t_deque *deque)
 	return (deque->front == -1);
 }
 
-void	deque_push_front(t_deque *deque, int data)
-{
-	if (deque_is_full(deque))
-		return ;
-	if (deque->front == 0)
-		deque->front = deque->sz - 1;
-	else
-		deque->front--;
-	deque->buffer[deque->front] = data;
-}
-
-void	deque_push_back(t_deque *deque, int data)
-{
-	if (deque_is_full(deque))
-		return ;
-	if (deque->back == deque->sz - 1)
-		deque->back = 0;
-	else
-		deque->back++;
-	deque->buffer[deque->back] = data;
-}
-
-int		*deque_pop_back(t_deque *deque)
-{
-	int		*data;
-
-	if (deque_is_empty(deque))
-		return (NULL);
-	data = &deque->buffer[deque->back];
-	if (deque->front == deque->back)
-	{
-		deque->front = -1;
-		deque->back = -1;
-	}
-	else if (deque->back == 0)
-		deque->back = deque->sz - 1;
-	else
-		deque->back--;
-	return (data);
-}
 
 void	deque_free_buffer(t_deque *deque)
 {
---
+++ src/deque_sub.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   deque_sub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkuramot <tkuramot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/09 23:07:43 by tkuramot          #+#    #+#             */
+/*   Updated: 2023/07/09 23:07:47 by tkuramot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	deque_push_front(t_deque *deque, int data)
+{
+	if (deque_is_full(deque))
+		return ;
+	if (deque->front == 0)
+		deque->front = deque->sz - 1;
+	else
+		deque->front--;
+	deque->buffer[deque->front] = data;
+}
+
+void	deque_push_back(t_deque *deque, int data)
+{
+	if (deque_is_full(deque))
+		return ;
+	if (deque->back == deque->sz - 1)
+		deque->back = 0;
+	else
+		deque->back++;
+	deque->buffer[deque->back] = data;
+}
+
+int		*deque_pop_front(t_deque *deque)
+{
+	int		*data;
+
+	if (deque_is_empty(deque))
+		return (NULL);
+	data = &deque->buffer[deque->front];
+	if (deque->front == deque->back)
+	{
+		deque->front = -1;
+		deque->back = -1;
+	}
+	else if (deque->front == deque->sz - 1)
+		deque->front = 0;
+	else
+		deque->front++;
+	return (data);
+}
+
+int		*deque_pop_back(t_deque *deque)
+{
+	int		*data;
+
+	if (deque_is_empty(deque))
+		return (NULL);
+	data = &deque->buffer[deque->back];
+	if (deque->front == deque->back)
+	{
+		deque->front = -1;
+		deque->back = -1;
+	}
+	else if (deque->back == 0)
+		deque->back = deque->sz - 1;
+	else
+		deque->back--;
+	return (data);
+}
